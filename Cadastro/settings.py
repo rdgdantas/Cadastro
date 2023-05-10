07384047d5eb8628c7aec5a9f55a0bfdf784d1af
Rodrigo Dantas <rodrigo_jhd@hotmail.com>
2023-05-10 19:18:17 -0300

    add pagina do site

--- Cadastro/settings.py
+++ Cadastro/settings.py
@@ -74,24 +74,24 @@ WSGI_APPLICATION = 'Cadastro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-#Mysql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'cadastro-cliente', 
-        'USER': 'root', 
-        'PASSWORD': '',
-        'HOST': 'localhost', 
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+#Mysql
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': 'cadastro-cliente', 
+#         'USER': 'root', 
+#         'PASSWORD': '',
+#         'HOST': 'localhost', 
+#         'PORT': '3306',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- CadastroCliente/views.py
+++ CadastroCliente/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index (request):
+    meu_nome = 'Ciclano da Silva'
+    lista_frutas = ['Laranja', 'Maça', 'Banana']
+    context = {
+
+        'nome': meu_nome,
+        'frutas': lista_frutas
+
+    }
+    return render(request, 'index.html', context)
